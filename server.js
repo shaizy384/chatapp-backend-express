35ca8238533b82bb38681ddd84bab7290fe0d27f
Shahzaib Qasim <shahzaibqasim384@gmail.com>
2023-12-27 12:35:21 +0500

    send event to every client

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 require('dotenv').config()
 
 const port = process.env.PORT || 2800
-
+const Frontend_HOST = process.env.Frontend_HOST
 connectDb()
 app.use(express.json())
 app.use(cors())
@@ -15,6 +15,18 @@ app.use('/api/chats', require("./routes/chats"))
 app.use('/api/message', require("./routes/message"))
 app.use('/api/friends', require("./routes/friends"))
 
-app.listen(port, ()=> {
+const server = app.listen(port, () => {
     console.log(`This app is listening on port ${port}`);
+})
+
+// socket io
+const io = require("socket.io")(server, {
+    cors: {
+        origin: Frontend_HOST
+    }
+})
+
+io.on("connection", (server)=>{
+    console.log("connected to socket.io");
+    io.emit("welcome", "hello this is socket io")
 })
