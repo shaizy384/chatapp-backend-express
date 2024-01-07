64c48abb27cdbd84ddc81f42d9ad8c12abbb3f45
Shahzaib Qasim <shahzaibqasim384@gmail.com>
2024-01-07 14:36:47 +0500

    implemented websocket

--- server.js
+++ server.js
@@ -26,7 +26,36 @@ const io = require("socket.io")(server, {
     }
 })
 
-io.on("connection", (server)=>{
+let users = [];     // stores online users
+io.on("connection", (socket) => {
     console.log("connected to socket.io");
-    io.emit("welcome", "hello this is socket io")
+    io.emit("welcome", "hello this is socket io");
+
+    // add user
+    socket.on("addUser", userId => {
+        //check user id, if not exist add in the array
+        console.log("a user connected");
+        userId && (!users.some(user => user.userId === userId) && users.push({ userId, socketId: socket.id }))
+        // console.log("userId: ", userId, users);
+        io.emit("getUsers", users);
+    });
+
+    // send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        // get specific user from array
+        const user = users.find(user => user.userId === receiverId)
+        console.log("user?.socketId: ", user);
+        io.to(user?.socketId).emit("getMessage", {
+            senderId, text,
+        })
+        // console.log("userId: ", users);
+        // io.emit("getUsers", users)
+    });
+
+    // disconnect user
+    socket.on("disconnect", () => {
+        console.log("a user disconnected");
+        users = users.filter(user => user?.socketId !== socket.id)
+        io.emit("getUsers", users)
+    });
 })
